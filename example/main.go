d8b4127e85347203eceeae87d55fbe398ae69a46
Simon Vans-Colina <simon@getmondo.co.uk>
2017-02-05 17:30:28 +0000

    remove hook url

--- example/main.go
+++ example/main.go
@@ -12,7 +12,7 @@ func main() {
 
 	w.Gates = []int{1, 10, 100, 500, 1000}                                                                    //alert me when each of these gates pass
 	w.TTL = 5 * time.Second                                                                                   //where the events happened in the last 30 minutes
-	w.Address, _ = url.Parse("https://hooks.slack.com/services/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") //by posting to this addresss
+	w.Address, _ = url.Parse("https://hooks.slack.com/services/webhookrulgoeshere") //by posting to this addresss
 	w.Description = "An Error was returned to GPS"
 
 	// a bad thing has happened.
