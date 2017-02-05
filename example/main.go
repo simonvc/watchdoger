675070cff96edc7a93c96618e2b490378ab640f8
Simon Vans-Colina <simon@getmondo.co.uk>
2017-02-05 13:53:48 +0000

    move main to example

---
+++ example/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/simonvc/watchdoger"
+)
+
+func main() {
+	var w watchdoger.Watch
+
+	w.Gates = []int{1, 10, 100, 500, 1000}                                                                    //alert me when each of these gates pass
+	w.TTL = 15 * time.Second                                                                                  //where the events happened in the last 30 minutes
+	w.Address, _ = url.Parse("https://hooks.slack.com/services/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") //by posting to this addresss
+	w.Description = "An Error was returned to GPS"
+
+	// a bad thing has happened.
+	w.Fire()
+
+	// another bad thing happens but no notify
+	w.Fire()
+
+	for i := 0; i < 2800; i++ {
+		w.Fire() //the 10th event should should also generate a notification
+	}
+
+	// now we sleep 31 seconds and watch the watch clear
+	// it should say All Clear: in the last 30 seconds there have been on 500 returned to GPS
+	time.Sleep(10 * time.Second)
+
+}
